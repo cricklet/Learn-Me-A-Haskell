05616bd425fb3905bbcdba655d79d4e08903ebed
Kenrick Rilee <kenrick.rilee@gmail.com>
2014-05-17 14:48:04 -0400

    Create a data type + value constructor. Create a type alias

--- driver.hs
+++ driver.hs
@@ -3,10 +3,16 @@ import Graphics.Gloss
 windowSize = 400
 renderScale = 5
 
-main
+main'
  = display window white draw
 
+main
+ = do putStrLn $ show testShape
+
 window = InWindow "Hello World" (windowSize, windowSize) (0, 0)
+
+type Point3D = (Float, Float, Float)
+data Shape = Sphere { center :: Point3D, radius :: Float } deriving (Show)
 
 rect :: Float -> Float -> Float -> Float -> Picture
 rect x0 y0 x1 y1
@@ -31,6 +37,9 @@ testPixels
   $ zipWith3 (\c x y -> coloredPixel c x y)
       [red, yellow, blue, green, black, orange]
       [1..5] [1..5]
+
+testShape
+  = Sphere { center = (0, 0, 10), radius = 5 }
 
 -- Use a resolution of 20 x 20 with 0,0 in the center
 
