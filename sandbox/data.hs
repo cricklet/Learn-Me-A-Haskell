baca691f5a33de64e83d2c8c9850048e11d26fb3
Kenrick Rilee <kenrick.rilee@gmail.com>
2014-08-19 21:48:13 -0400

    Think I've got data types down now...

--- sandbox/data.hs
+++ sandbox/data.hs
@@ -17,8 +17,13 @@ showCharacter (Elf e) = "Elf: " ++ e
 showCharacter (Hobbit h) = "Hobbit: " ++ h
 
 data List a = Empty | Cons a (List a)
-showList1 (Cons x xs) = x ++ "," ++ showList1 xs
+showList1 :: Show a => List a -> String
+showList1 (Cons x xs) = (show x) ++ "," ++ showList1 xs
 showList1 Empty = ""
+
+data Tree a = Leaf | Node a (Tree a) (Tree a) deriving (Eq, Ord, Show)
+treeFromList (x:xs) = Node x (treeFromList $ filter (<x) xs) (treeFromList $ filter (>x) xs)
+treeFromList [] = Leaf
 
 main =
   do let name = "Kenrick"::Name
@@ -27,16 +32,15 @@ main =
          size = SizeConstr "big"
          legolas = Elf { weapon="bow" }
          bilbo = Hobbit { food="pastries" }
-         empty = Empty
-         a = Cons "a" empty
-         ab = Cons "b" a
-         abc = Cons "c" ab
+         a = Cons 1 Empty
+         ab = Cons 2 a
+         abc = Cons 3 ab
 
      print $ showInfo name color
      print $ showFruit fruit size
      print $ showCharacter legolas
      print $ showCharacter bilbo
-     print $ showList1 empty
      print $ showList1 a
      print $ showList1 ab
      print $ showList1 abc
+     print $ treeFromList [4,6,2,3,7,4,8,1]
