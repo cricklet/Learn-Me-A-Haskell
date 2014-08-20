15e4c60c2a7ccf64603cd5f687993527c68ac3c8
Kenrick Rilee <kenrick.rilee@gmail.com>
2014-08-19 22:19:02 -0400

    IO

--- sandbox/io.hs
+++ sandbox/io.hs
@@ -1,8 +1,15 @@
-main = do
-  putStrLn "What's your name?" -- IO ()
-  name <- getLine -- IO String
-  putStrLn $ "Hi, " ++ name
+maybeRead :: Read a => String -> Maybe a
+maybeRead s = case reads s of
+                  [(x,"")] -> Just x
+                  _        -> Nothing
+
+doubleAge = do
   putStrLn "What's your age?"
   ageStr <- getLine
-  let age = read ageStr
-  putStrLn $ "Your age times 2 is: " ++ show (age * 2)
+  let age = maybeRead ageStr in
+    case age of
+      Just a  -> putStrLn $ "Your age times 2 is: " ++ show (a * 2)
+      Nothing -> doubleAge
+
+main = do
+  doubleAge
